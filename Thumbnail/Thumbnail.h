1f23b5f1e1c8556ceb7c8273c9abe835b21c377e
isadiliballi <isadiliballi@icloud.com>
2021-06-12 23:00:28 +0300

    The project has been uploaded to GitHub for the first time.

---
+++ Thumbnail/Thumbnail.h
@@ -0,0 +1,18 @@
+//
+//  Thumbnail.h
+//  Thumbnail
+//
+//  Created by İsa Diliballı on 12.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Thumbnail.
+FOUNDATION_EXPORT double ThumbnailVersionNumber;
+
+//! Project version string for Thumbnail.
+FOUNDATION_EXPORT const unsigned char ThumbnailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thumbnail/PublicHeader.h>
+
+
